02ef3e9b57ac29c2b6e6edff9483c5c8180f101b
Maxim Vasilyev <cyberenergy716@gmail.com>
2022-08-11 14:17:44 +0300

    Deleted unused validation method

--- new_contact_test.py
+++ new_contact_test.py
@@ -112,17 +112,7 @@ class NewContactTest(unittest.TestCase):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
+
     
     def tearDown(self):
         self.wd.quit()
