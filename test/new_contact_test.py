9924d2c989d75493811ddf295236d820ab2b6e1d
Maxim Vasilyev <cyberenergy716@gmail.com>
2022-08-19 15:29:27 +0300

    Created new module contact.py for methods decomposition.

--- test/new_contact_test.py
+++ test/new_contact_test.py
@@ -11,12 +11,12 @@ def app(request):
 
 def test_new_contact(app):
     app.open_test_page()
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Maxim", middlename="Olegovich", lastname="Vasilyev", nickname="MaXj", title="Genius", company="TechArgos", address="Novodmitrovskaya 2b",
-                        home_phone="-", cell_phone="70418041804", work_phone="aslfalf", fax="fjafafasj", email_1="vasiafaf@gmail.com", email_2="fjajfafs@yandex.ru",
-                        email_3="kfjajfakfa@rambler.com", homepage="asfakjfla.ru", day="15", month="November", year="1998", a_day="16", a_month="July", a_year="1987",
-                        address2="zxvkzlvjz", secondary_phone="afkjalfjaljf", notes="ksdljgslgjksl"))
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Maxim", middlename="Olegovich", lastname="Vasilyev", nickname="MaXj", title="Genius", company="TechArgos", address="Novodmitrovskaya 2b",
+                               home_phone="-", cell_phone="70418041804", work_phone="aslfalf", fax="fjafafasj", email_1="vasiafaf@gmail.com", email_2="fjajfafs@yandex.ru",
+                               email_3="kfjajfakfa@rambler.com", homepage="asfakjfla.ru", day="15", month="November", year="1998", a_day="16", a_month="July", a_year="1987",
+                               address2="zxvkzlvjz", secondary_phone="afkjalfjaljf", notes="ksdljgslgjksl"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
 
