c817d85d93a6ba033364502520410d6574860692
Maxim Vasilyev <cyberenergy716@gmail.com>
2022-08-30 12:11:43 +0300

    Created cache storage for group_list parsing.

--- fixture/group.py
+++ fixture/group.py
@@ -18,6 +18,8 @@ class GroupHelper:
         # Submit creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
+
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -46,6 +48,7 @@ class GroupHelper:
         # Submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -62,6 +65,7 @@ class GroupHelper:
         # Submit changes
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
 
     def count(self):
@@ -69,12 +73,16 @@ class GroupHelper:
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+
+
+    group_cache = None
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_group_page()
-        group_list = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            group_list.append(Group(name = text, id = id))
-        return group_list
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_group_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name = text, id = id))
+        return list(self.group_cache)
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -6,7 +6,7 @@ def test_del_group(app):
         app.group.create(Group(name = "test"))
     old_group_list = app.group.get_group_list()
     app.group.delete()
+    assert len(old_group_list) - 1 == app.group.count()
     new_group_list = app.group.get_group_list()
-    assert len(old_group_list) - 1 == len(new_group_list)
     old_group_list[0:1] = []
     assert old_group_list == new_group_list
