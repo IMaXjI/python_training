e3faf508839c7ce55ddfe883fffb070527892b2a
Vasilyev Maxim <vasilyevmaxi@gmail.com>
2022-09-07 23:34:26 +0300

    Created random data flow for group_creation_test

--- test/group_creation_test.py
+++ test/group_creation_test.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import random
+import string
+import pytest
 
 
-def test_group_creation(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 10)]
+    for footer in ["", random_string("footer", 10)]
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_group_creation(app, group):
     old_group_list = app.group.get_group_list()
-    group = Group(name="group", header="afafasf", footer="aSFAF")
     app.group.create(group)
     assert len(old_group_list) + 1 == app.group.count()
     new_group_list = app.group.get_group_list()
     old_group_list.append(group)
     assert sorted(old_group_list, key = Group.id_or_max) == sorted(new_group_list, key = Group.id_or_max)
 
-# def test_empty_group_creation(app):
-#     old_group_list = app.group.get_group_list()
-#     group = Group(name="group", header="afafasf", footer="aSFAF")
-#     app.group.create(group)
-#     new_group_list = app.group.get_group_list()
-#     assert len(old_group_list) + 1 == len(app.group.count())
-#     old_group_list.append(group)
-#     assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
+
