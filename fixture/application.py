3c19f9fe93ff1022a5fb3b0f2943460e96f57fe5
Maxim Vasilyev <cyberenergy716@gmail.com>
2022-09-08 15:43:17 +0300

    Created terminal parameters with default browser and application url options.

--- fixture/application.py
+++ fixture/application.py
@@ -5,12 +5,20 @@ from fixture.group import GroupHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognised browser argument: %s" % browser)
         self.wd.implicitly_wait(3)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
