66c1c83bf50e85dc0581c8a541ba36951c575eb8
Maxim Vasilyev <cyberenergy716@gmail.com>
2022-09-20 11:54:19 +0300

    Refactored conftest fixture. Added optional --check_ui argument

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,7 +4,7 @@ from model.group import Group
 import random
 
 
-def test_del_group(app, db):
+def test_del_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name = "test"))
     old_group_list = db.get_group_list()
@@ -14,3 +14,6 @@ def test_del_group(app, db):
     assert len(old_group_list) - 1 == app.group.count()
     old_group_list.remove(group)
     assert old_group_list == new_group_list
+    if check_ui:
+        assert sorted(new_group_list, key = Group.id_or_max)  == sorted(app.group.get_group_list(), key = Group.id_or_max)
+
