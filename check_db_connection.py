16d6744503b9ffd7b555e0ac3caba9eb3f9a1935
Maxim Vasilyev <cyberenergy716@gmail.com>
2022-09-21 15:40:36 +0300

    Created relations between groups and contacts for complex orm queries

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,12 @@
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="172.17.41.29", name="addressbook", user="admin", password="secret")
 # connection = pymysql.connect(host="172.17.41.29", database="addressbook", user="admin", password="secret")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_in_group(Group(id="5"))
     for item in l:
         print(item)
     print(len(l))
