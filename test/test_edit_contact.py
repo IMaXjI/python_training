df9108591772a4995114247874ae73ae3df77a6f
Maxim Vasilyev <cyberenergy716@gmail.com>
2022-10-04 11:12:38 +0300

    Refactored test_edit_contact test

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -7,12 +7,12 @@ def test_edit_contact_name(app, db, check_ui):
         app.contact.create(Contact(firstname="Test"))
     old_contact_list = db.get_contact_list()
     contact = random.choice(old_contact_list)
-    new_data = Contact(firstname="TEST WORKS", lastname="TEST WORKS")
+    new_data = Contact(firstname="Changed", lastname="VALUE")
     app.contact.edit_by_id(contact.id, new_data)
     new_contact_list = db.get_contact_list()
     for item in old_contact_list:
         if item.id == contact.id:
-            item.name = new_data.firstname
+            item.firstname = new_data.firstname
             item.lastname = new_data.lastname
     assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
     if check_ui:
